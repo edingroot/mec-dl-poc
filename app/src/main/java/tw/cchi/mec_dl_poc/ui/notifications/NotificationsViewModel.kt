d7ffc2193d03a2ddc25c0841c10849c74101509d
Chi Chang <edingroot@gmail.com>
2019-09-08 23:06:01 -0500

    Initial commit with android sample navigation app

---
+++ app/src/main/java/tw/cchi/mec_dl_poc/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package tw.cchi.mec_dl_poc.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
